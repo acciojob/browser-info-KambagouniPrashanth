59868ab15cf42ffe808e95e5e3e66a4d35adfd2a
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-07-01 07:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const divelemnt=document.getElementById("browser-info")
+divelemnt.innerText="You are using " + navigator.userAgent + " version " + navigator.appVersion;
